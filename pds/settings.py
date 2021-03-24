e176e2252f827f0dcf20b2f43b05b35534043ad7
kayshad <kaisslama@gmail.com>
2021-03-24 18:46:35 +0100

    Update settings.py

--- pds/settings.py
+++ pds/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's30*2uxz#6$^%foek%q=on04vpzq*_8mbg*fjc(6-^n8=-mu-q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
